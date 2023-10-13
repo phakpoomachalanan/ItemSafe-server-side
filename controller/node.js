a853f855c979276d341add6007c61e5aea837673
Phakpoom Achalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-10-13 23:13:18 +0700

    function template

---
+++ controller/node.js
@@ -0,0 +1,22 @@
+import neo4j from 'neo4j-driver'
+import { driver } from '../util/connectDb.js'
+
+export function createNode() {
+
+}
+
+export function updateNode() {
+
+}
+
+export function deleteNode() {
+
+}
+
+export function findNode() {
+
+}
+
+export function getChildren() {
+    
+}
