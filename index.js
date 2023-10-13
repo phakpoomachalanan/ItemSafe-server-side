5ec6b068deb83fa50ea049cc7ee6d8ac0909af75
Phakpoom Achalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-10-13 20:48:38 +0700

    add logger

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 import dotenv from 'dotenv'
 import { connectDb } from './util/connectDb.js'
 import createApp from './app.js'
+import { logger } from './util/logger.js';
 
 
 function main() {
     dotenv.config();
     const app = createApp()
 
-    const PORT = process.env.PORT || 3000;
+    const PORT = process.env.APP_PORT || 3000;
     const server = app.listen(PORT, () => {
+        logger.info(`server start at http://localhost:${PORT}`)
         connectDb()
     });
-
 
     process.on('unhandledRejection', (err) => {
         console.error(err.name, err.message);
