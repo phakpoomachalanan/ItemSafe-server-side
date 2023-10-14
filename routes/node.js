b5d63194c66e9341f891d5c4e9f775cfecd4912e
Phakpoom Achalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-10-14 22:30:36 +0700

    route

--- routes/node.js
+++ routes/node.js
@@ -1,9 +1,16 @@
 import { Router } from "express"
-import { getChildren } from "../controller/node.js"
+import { createNode, deleteNode, getAttribute, getChildren, updateNode } from "../controller/node.js"
 
 
 const router = Router()
 
-router.get("/children", getChildren)
+router.post("/create", createNode)
+
+router.put("/{nodeId}/update", updateNode)
+
+router.get("/{nodeId}/attr", getAttribute)
+router.get("/{nodeId}/children", getChildren)
+
+router.delete("/{nodeId}/delete", deleteNode)
 
 export default router
--- routes/tree.js
+++ routes/tree.js
@@ -1,9 +1,13 @@
 import { Router } from "express"
-import { getTree } from "../controller/tree.js"
+import { getTree, initTree, updateTree } from "../controller/tree.js"
 
 
 const router = Router()
 
-router.get("/tree", getTree)
+router.post("/init/{:root}/tree", initTree)
+
+router.put("/{:root}/tree", updateTree)
+
+router.get("/{:root}/tree", getTree)
 
 export default router
