fd28f4aac037786c5111b7f7555a0df5228260b4
Phakpoom Achalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-10-15 06:52:52 +0700

    change func

--- routes/item.js
+++ routes/item.js
@@ -1,11 +1,11 @@
 import { Router } from "express"
-import { createItem, deleteItem, getAllItem, getChildren, getItem, getParent, updateItem } from "../controller/item.js"
+import { createItem, deleteItem, getAllItem, getChildren, getItem, getParent, updateItem, uploadItem } from "../controller/item.js"
 
 
 const router = Router()
 
 router.post("/create", createItem)
-router.post("/upload", updateItem)
+router.post("/upload", uploadItem)
 
 router.put("/:itemId/update", updateItem)
 
