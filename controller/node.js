90671164c295ac27d857a7c0e2c015cff75adfe4
Phakpoom Achalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-10-15 05:11:06 +0700

    set zero

--- controller/node.js
+++ controller/node.js
@@ -1,24 +1,7 @@
-import { driver } from '../util/connectDb.js'
-import { defaultNodeSchema } from '../model/node.js';
+
 
 
 export const createNode = (req, res, next) => {
-    const params = req.body
-    const session = driver.session()
-
-    session
-    .run(defaultNodeSchema, params)
-    .then(result => {
-        const createdNode = result.records[0].get('item')
-        console.log('Created node:', createdNode.properties)
-        return res.json(createdNode.properties)
-    })
-    .catch(error => {
-        next(error)
-    })
-    .finally(() => {
-        session.close()
-    })
 
 }
 
