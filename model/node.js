7965eb2bb9a452e1cd84662791c67235050efa31
Phakpoom Achalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-10-14 22:28:13 +0700

    model

--- model/node.js
+++ model/node.js
@@ -1,21 +1,13 @@
 export const defaultNodeSchema = `
     CREATE (item:Item {
         name: $name,
+        description: $description,
+        type: $type,
+        size: $size,
+        path: $path,
+        cover: $cover,
         warnings: $warnings,
         tags: $tags
     })
     RETURN item
 `
-// export const defaultNodeSchema = `
-//     CREATE (item:Item {
-//         name: $name,
-//         description: $description,
-//         type: $type,
-//         size: $size,
-//         path: $path,
-//         cover: $cover,
-//         warnings: $warnings,
-//         tags: $tags
-//     })
-//     RETURN item
-// `
