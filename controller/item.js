1caf82a2f9c56683b13f818f2f0bc10e6ad3d05a
Phakpoom Achalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-10-15 05:42:21 +0700

    create item

--- controller/item.js
+++ controller/item.js
@@ -1,22 +1,44 @@
+import Item from '../model/item.js'
 
 
+export const createItem = async (req, res, next) => {
+    const { name, description, type, size, path, cover, parent, warnings, tags } = req.body
 
-export const createItem = (req, res, next) => {
+    let itemDetail = {
+        name: name,
+        description: description,
+        type: type,
+        size: size,
+        path: path,
+        warnings: warnings,
+        tags: tags,
+    }
+
+    if (parent !== "") {
+        itemDetail.parent = parent
+    }
+
+    if (cover !== "") {
+        itemDetail.cover = cover
+    }
+
+    const item = await Item.create(itemDetail)
+
+    return res.json(item)
+}
+
+export const updateItem = async (req, res, next) =>{
+}
+
+
+export const deleteItem = async (req, res, next) => {
 
 }
 
-export const updateItem = (req, res, next) =>{
-
-}
-
-export const deleteItem = (req, res, next) => {
-
-}
-
-export const getChildren = (req, res, next) => {
+export const getChildren = async (req, res, next) => {
     
 }
 
-export const getItem = (req, res, next) => {
+export const getItem = async (req, res, next) => {
 
 }
