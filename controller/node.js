43a87bb0b9644e88b464023aac59b440b00e42e6
Phakpoom Achalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-10-14 22:23:15 +0700

    create node

--- controller/node.js
+++ controller/node.js
@@ -1,8 +1,24 @@
-import neo4j from 'neo4j-driver'
-import driver from '../util/connectDb.js'
+import { driver } from '../util/connectDb.js'
+import { defaultNodeSchema } from '../model/node.js';
 
 
 export const createNode = (req, res, next) => {
+    const params = req.body
+    const session = driver.session()
+
+    session
+    .run(defaultNodeSchema, params)
+    .then(result => {
+        const createdNode = result.records[0].get('item')
+        console.log('Created node:', createdNode.properties)
+        return res.json(createdNode.properties)
+    })
+    .catch(error => {
+        next(error)
+    })
+    .finally(() => {
+        session.close()
+    })
 
 }
 
