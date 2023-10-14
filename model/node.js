04f3a738c12bedcd571fe096a80079f51bf5f333
Phakpoom Achalanan <107357501+phakpoomachalanan@users.noreply.github.com>
2023-10-15 05:10:37 +0700

    schema for item

--- model/node.js
+++ model/node.js
@@ -1,13 +1,37 @@
-export const defaultNodeSchema = `
-    CREATE (item:Item {
-        name: $name,
-        description: $description,
-        type: $type,
-        size: $size,
-        path: $path,
-        cover: $cover,
-        warnings: $warnings,
-        tags: $tags
-    })
-    RETURN item
-`
+import mongoose from "mongoose";
+
+const itemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        require: true,
+    },
+    description: {
+        type: String,
+    },
+    type: {
+        type: String,
+        require: true,
+    },
+    size: {
+        type: String,
+        require: true,
+    },
+    path: {
+        type: String,
+        require: true,
+    },
+    cover: {
+        type: String,
+    },
+    warnings: [{
+        type: mongoose.Types.ObjectId,
+        ref: "Warning"
+    }],
+    tags: [{
+        type: mongoose.Types.ObjectId,
+        ref: "Tag"
+    }]
+})
+
+const Item = mongoose.models.item || mongoose.model("Item", itemSchema)
+export default Item
